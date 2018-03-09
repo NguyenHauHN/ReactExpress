5ca80521f84b5db54141f44844bf21346ec27641
Nguyen Thi Hau <haunt@gmail.com>
2018-03-09 09:12:20 +0700

    example conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Card extends Component{
+  render(){
+    const {title, subtitle} = this.props
+    return (
+      <div style={styles.card}>
+        <h1 style={styles.title}>{title}</h1>
+        { subtitle &&
+        (<h2 style={styles.subtitle}>{subtitle}</h2>)
+        }
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card title={'Title'}/>
+        <Card title={'Title'} subtitle={'Subtitle'}/>
       </div>
     );
   }
+}
+
+const styles = {
+  card: {
+    padding: 20,
+    margin: 20,
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: 'skyblue',
+    border: '1px solid rgba(0,0,0,0.15)'
+  },
+  title: {
+    fontSize: '18',
+    lineHeight: '24px'
+  },
+  subtitle: {
+    fontSize: 14,
+    lineHeight: '18px'
+  },
 }
 
 export default App;
