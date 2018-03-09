d926afb9c1266e7ff6f5517ae730b00b5f9e1a41
Nguyen Thi Hau <haunt@gmail.com>
2018-03-09 14:15:52 +0700

    redux demo 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {createStore} from 'redux';
 import logo from './logo.svg';
 import './App.css';
+
+// define action type
+const types = {
+  INCREMENT: 'INCREMENT'
+}
+
+// define a reducer
+const reducer = (state, action) => {
+  if (action.type == types.INCREMENT) {
+    return {count: state.count + 1}
+  }
+
+  return state
+}
+
+// define initial state of store
+const initialState = {count: 0}
+
+// define a store
+const store = createStore(reducer, initialState)
+
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+store.dispatch({type: types.INCREMENT})
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{fontSize: 100}}>
+        {store.getState().count}
       </div>
     );
   }
